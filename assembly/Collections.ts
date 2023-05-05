f015f687c73b6b15bf36b11c4364e955bbd94c00
koinospunks <koinospunks@outlook.com>
2023-05-05 10:16:47 +0200

    increased amount of tokens allowed for an address

--- assembly/Collections.ts
+++ assembly/Collections.ts
@@ -184,7 +184,7 @@ export class Collections {
     balance.value = SafeMath.add(balance.value, args.number_tokens_to_mint);
 
     // check limit address
-    System.require(balance.value <= 201, "exceeds the limit of tokens per address");
+    System.require(balance.value < 201, "exceeds the limit of tokens per address");
 
     // increment supply
     supply.value = SafeMath.add(supply.value, args.number_tokens_to_mint);
